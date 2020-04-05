57f2226019029aea9475f0089f2c815a6c33553e
徳久　貴裕 <tokuhisatakahiro@tokuhisatakahironoMacBook-Pro.local>
2020-04-05 12:29:29 +0900

    Q11,13修正

--- task.rb
+++ task.rb
@@ -110,7 +110,7 @@ def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-  sports = sports.flatten.uniq
+  sports.flatten!.uniq!
   
   puts "ユーザーの趣味一覧"
   sports.each.with_index(1) do |sports,i| 
@@ -131,8 +131,7 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
-  user_data.store(:age, 32)
-  user_data.store(:address, "沖縄")
+  user_data.update(update_data)
     puts user_data
 end
 
